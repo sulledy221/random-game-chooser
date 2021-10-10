c9a355a0cf163bdf1e4885e1329b2cca2d7107f2
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-10-10 14:18:38 -0700

    start button functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./all.css";
+import { useState } from "react";
 
 export default function Button() {
-  return <div>Need help deciding what to play? Click the button!</div>;
+  const [button, setButton] = useState("");
+  return (
+    <div>
+      <button onClick={setButton}>
+        Need help deciding what to play? Click the button!
+      </button>
+    </div>
+  );
 }
